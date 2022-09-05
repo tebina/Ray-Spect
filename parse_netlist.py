68f87c797b7336e489603c4fe9cf1a9c77e97051
Tebina <tebina1@live.fr>
2022-09-05 17:34:21 +0200

    Subcircuit parse working

--- parse_netlist.py
+++ parse_netlist.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+
 ppt = pp.testing
 
 
@@ -11,22 +12,47 @@ def parse_netlist(file_string):
     eol = pp.lineEnd().suppress()  # End of line
     line_break = pp.Suppress("\\" + pp.LineEnd())  # Line break
     equal_sign = pp.Suppress("=")
-    circuit_end = pp.Keyword("ends").suppress()  # Circuit end statement
-    identifier = pp.Word(pp.alphanums + '_!<>') # a word containing letters and numbers
-    expression = pp.Word(pp.alphanums+'._*+-/()') # any expression, can be a float with negative or an equation
+    open_parenthesis = pp.Suppress("(")
+    close_parenthesis = pp.Suppress(")")
+
+    identifier = pp.Word(pp.alphanums + '_!<>')  # a word containing letters and numbers
+    expression = pp.Word(pp.alphanums + '._*+-/()')  # any expression, can be a float with negative or an equation
     comment = pp.Suppress("//" + pp.SkipTo(pp.LineEnd()))  # Skipping comments
 
     # Handling parameters of any type <Var = Value>
-    inst_param_name = identifier
-    inst_param_value = expression
-    inst_parameter = pp.Group(inst_param_name('name') + equal_sign + inst_param_value('value')).setResultsName('key').set_debug()
+    param_name = identifier
+    param_value = expression
+    inst_parameter = pp.Group(param_name('name') + equal_sign + param_value('value')).setResultsName(
+        'parameter')
+    many_parameters = pp.Group(pp.ZeroOrMore(inst_parameter | line_break)).setResultsName('parameters')
+
+    # Handling nets
+    net = identifier
+    nets = pp.Group(pp.OneOrMore(net('net') | line_break))
 
     # Handling parameter definition line which always starts with "parameters" and ends with a line break
-    parameter_line = pp.Group('parameters' + pp.ZeroOrMore(inst_parameter | line_break)).setResultsName('parameters_line')
+    parameter_line = pp.Group(pp.Keyword("parameters").suppress() + many_parameters).setResultsName(
+        'parameters_line')
 
+    # Instance handling
+    instance_name = identifier
+    parent_instance = identifier
+    instance = pp.Group(instance_name('name') + open_parenthesis + nets('nets') + close_parenthesis + parent_instance(
+        'parent') + many_parameters + eol)
 
+    # Sub-circuit description handling
+    subcircuit_name = identifier
+    subcircuit_end = pp.Keyword("ends").suppress()  # Circuit end statement
+    subcircuit_content = pp.Group(pp.ZeroOrMore(parameter_line | instance | eol))
+    subcircuit = pp.Group(
+        # content matches ==> subckt <name> <nets> <eol>
+        pp.Keyword("subckt").suppress() + subcircuit_name('name') + nets('nets') + eol
+        # content matches ==> parameters_line + instances | instances
+        + subcircuit_content
+        # content matches ==> ends <name> <eol>
+        + subcircuit_end + pp.matchPreviousExpr(subcircuit_name).suppress() + eol).setResultsName('subcircuit')
 
-    netlist_element = (eol | comment | circuit_end | inst_parameter | parameter_line)
+    netlist_element = (eol | comment | subcircuit)
     print(ppt.with_line_numbers(file_string))
 
     netlist = pp.ZeroOrMore(netlist_element) + pp.StringEnd()
