33935c8411155942a468d5f8337ba3bcaa6639da
Tebina <tebina1@live.fr>
2022-09-05 16:48:29 +0200

    Fixed parameter line parsing and multiple parameter parse working !

--- parse_netlist.py
+++ parse_netlist.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+ppt = pp.testing
 
 
 def parse_netlist(file_string):
@@ -9,21 +10,25 @@ def parse_netlist(file_string):
     # Specter netlist grammar
     eol = pp.lineEnd().suppress()  # End of line
     line_break = pp.Suppress("\\" + pp.LineEnd())  # Line break
-    identifier = pp.Word(pp.alphanums + '_!<>')  # a word containing letters and numbers
-    number = pp.Word(pp.nums + ".ue-")  # a number, can be float
-    expression = pp.Word(pp.alphanums + '._*+-/()')
-    comment = pp.Suppress("//" + pp.SkipTo(pp.LineEnd()))  # Skipping comments
-    net = identifier
-    nets = pp.Group(pp.OneOrMore(net('net') | line_break))  # many nets
-    circuit = identifier  # a net is an identifier
+    equal_sign = pp.Suppress("=")
     circuit_end = pp.Keyword("ends").suppress()  # Circuit end statement
-    inst_param_name = expression
+    identifier = pp.Word(pp.alphanums + '_!<>') # a word containing letters and numbers
+    expression = pp.Word(pp.alphanums+'._*+-/()') # any expression, can be a float with negative or an equation
+    comment = pp.Suppress("//" + pp.SkipTo(pp.LineEnd()))  # Skipping comments
 
-    inst_param_value = pp.Suppress("=") + number  # value of a param, can contain exp and negative exp and the unit
-    inst_parameter = pp.Group(inst_param_name('name') + inst_param_value('value')).setResultsName('key')
-    parameters = pp.Group(pp.ZeroOrMore(inst_parameter | line_break)).setResultsName('parameters')
+    # Handling parameters of any type <Var = Value>
+    inst_param_name = identifier
+    inst_param_value = expression
+    inst_parameter = pp.Group(inst_param_name('name') + equal_sign + inst_param_value('value')).setResultsName('key').set_debug()
 
-    netlist_element = number | identifier | eol | comment | circuit_end | parameters | expression
+    # Handling parameter definition line which always starts with "parameters" and ends with a line break
+    parameter_line = pp.Group('parameters' + pp.ZeroOrMore(inst_parameter | line_break)).setResultsName('parameters_line')
+
+
+
+    netlist_element = (eol | comment | circuit_end | inst_parameter | parameter_line)
+    print(ppt.with_line_numbers(file_string))
+
     netlist = pp.ZeroOrMore(netlist_element) + pp.StringEnd()
 
     return netlist.parseString(file_string)
