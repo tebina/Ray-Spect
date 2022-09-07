e571567fd68109f042456ccde16f65d0ed3daaf3
Tebina <tebina1@live.fr>
2022-09-07 18:20:20 +0200

    parameters line distinguished

--- parse_netlist.py
+++ parse_netlist.py
@@ -1,5 +1,5 @@
 import pyparsing as pp
-import results_handler as nl
+import results_handler as rh
 
 
 def parse_netlist(file_string):
@@ -23,15 +23,15 @@ def parse_netlist(file_string):
     param_value = expression
     inst_parameter = pp.Group(param_name('name') + equal_sign + param_value('value')).setResultsName('key')
     many_parameters = pp.Group(pp.ZeroOrMore(inst_parameter | line_break)).setResultsName(
-        'parameters')
+        'many_parameters')
 
     # Handling nets
     net = identifier
     nets = pp.Group(pp.OneOrMore(net('net') | line_break))
 
     # Handling parameter definition line which always starts with "parameters" and ends with a line break
-    parameter_line = pp.Group(pp.Keyword("parameters").suppress() + many_parameters).setResultsName(
-        'line_parameters')
+    parameters = pp.Group(pp.Keyword("parameters").suppress() + many_parameters).setResultsName(
+        'parameters')
 
     # Instance handling
     instance_name = identifier
@@ -42,31 +42,31 @@ def parse_netlist(file_string):
     # Sub-circuit description handling
     subcircuit_name = identifier
     subcircuit_end = pp.Keyword("ends").suppress()  # Circuit end statement
-    subcircuit_content = pp.Group(pp.ZeroOrMore(parameter_line | instance | eol)).setResultsName('subnetlist')
+    subcircuit_content = pp.Group(pp.ZeroOrMore(instance | eol)).setResultsName('subnetlist')
     subcircuit = pp.Group(
         # content matches ==> subckt <name> <nets> <eol>
         pp.Keyword("subckt").suppress() + subcircuit_name('name') + nets('nets') + eol
+        # content matches the parameters line
+        + pp.ZeroOrMore(parameters)
         # content matches ==> parameters_line + instances | instances
         + subcircuit_content
         # content matches ==> ends <name> <eol>
         + subcircuit_end + pp.matchPreviousExpr(subcircuit_name).suppress() + eol).setResultsName('subcircuit')
 
-    #subcircuit.setParseAction(handle_subcircuit)
+    subcircuit.setParseAction(handle_subcircuit)
 
     netlist_element = (eol | comment | subcircuit)
     netlist = pp.ZeroOrMore(netlist_element) + pp.StringEnd()
-
-    # parameter_line.setParseAction(handle_parameters_line)
 
     return netlist.parseString(file_string)
 
 
 def handle_subcircuit(token):
     sc = token.subcircuit
-    nets = sc.nets
     name = sc.name
     instances = sc.subnetlist
-    s = nl.SubCircuit(name, nets, instances)
+    parameters = sc.parameters
+    s = rh.SubCircuit(name, instances, parameters)
     return [s]
 
 
@@ -75,7 +75,7 @@ sample = file.read()
 
 # parse the netlist
 parsed_netlist = parse_netlist(sample)
-print(parsed_netlist)
+#print(parsed_netlist)
 
 
-print(parsed_netlist[0].parent)
+print(parsed_netlist[2].parameters)
