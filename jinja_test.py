2aa7109b784396680cb7a9d9a888071e86f44dd5
Tebina <tebina1@live.fr>
2022-09-16 15:44:08 +0200

    plotting a graph with an instance list as a starting point working

--- jinja_test.py
+++ jinja_test.py
@@ -2,8 +2,8 @@ import jinja2
 from parsers.netlist_parser import *
 import networkx as nx
 import matplotlib.pyplot as plt
-G = nx.Graph()
 
+G = nx.DiGraph()
 
 file = open('netlist/netlist', 'r')
 sample = file.read()
@@ -11,18 +11,50 @@ sample = file.read()
 parsed_netlist = parse_netlist(sample)
 # for i in range(len(parsed_netlist)):
 
+
+insts = ["g50488__4296", "g50534__1786", "g50439__1786", "g50930", "g51020__6877", "g50475__2250", "g50946__7118" , "g50990__5703" , "g50801__1857"]
 for component in parsed_netlist:
     if component.typeof == "SubCircuit":
-        for eachinstance in component.instances:
-            G.add_edge(component.name, eachinstance.name)
-            G.add_edge(eachinstance.name, eachinstance.parent)
 
-H = nx.DiGraph(G)
-subax1 = plt.figure(1)
-nx.draw(H)
+        if component.name == "sbox":
+            for eachinstance in component.instances:
+                for myinsts in insts:
+                    if myinsts == eachinstance.name:
+                        G.add_edge(myinsts, eachinstance.parent, color='r', weight=1)
+        elif component.name != "sbox_1" and component.name != "sbox_2":
+            for eachinstance in component.instances:
+                #print (component.name, "=====>", eachinstance.parent)
+                G.add_edge(component.name, eachinstance.parent, color='b', weight=0.5)
+
+
+
+# print(list(G.successors("NAND22")))
+
+
+
+print (list(nx.dfs_tree(G,"g50534__1786").edges()))
+
+print (list(G.nodes))
+
+colors = nx.get_edge_attributes(G,'color').values()
+weights = nx.get_edge_attributes(G,'weight').values()
+plt.figure(1,figsize=(20,20))
+pos = nx.circular_layout(G)
+nx.draw(G, pos,
+        edge_color=colors,
+        node_size=1000,
+        width=list(weights),
+        with_labels=True,
+        node_color='lightgreen')
+plt.savefig("graph.pdf")
 plt.show()
 
-
+# plt.figure(figsize=(9, 9))
+# nx.draw_spring(G, with_labels=True)
+# plt.savefig("graph.pdf")
+# plt.axis("equal")
+# plt.show()
+# #
 
 
 # templateLoader = jinja2.FileSystemLoader(searchpath="./")
